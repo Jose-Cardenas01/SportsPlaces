5d058d4431bbb3a8838c0b67a6ba43964bf927ee
Jose Cardenas <josecardenas0045@gmail.com>
2026-04-23 19:35:17 -0500

    adjustmen in notificaciones

--- SportsPlacesWeb/Data/Entity/Notificacione.cs
+++ SportsPlacesWeb/Data/Entity/Notificacione.cs
@@ -1,19 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace SportsPlacesWeb.Data.Entity;
 
 public partial class Notificacione
 {
-    public int Id { get; set; }
-
-    public string Mensaje { get; set; } = null!;
-
+    [Required]
+    public required int Id { get; set; }
+    [Required]
+    [MaxLength(500, ErrorMessage = "El {0} no puede exceder los {1} caracteres")]
+    public required string Mensaje { get; set; } = null!;
     public DateTime? FechaEnvio { get; set; }
-
-    public int UsuarioId { get; set; }
-
     public string? TipoNotificacion { get; set; }
+    // FK
+    [Required]
+    public required int UsuarioId { get; set; }
 
+    // Navigation Property
     public virtual Usuario Usuario { get; set; } = null!;
 }
